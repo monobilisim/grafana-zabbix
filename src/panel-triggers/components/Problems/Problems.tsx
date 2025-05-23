1d49f8deaabfc63b00dd8bdf63769cc99d5eb1e7
Ufuk Furkan Öztürk <ufuk.ozturk@mono.tr>
2025-05-23 10:52:56 +0300

    Update Problems.tsx

--- src/panel-triggers/components/Problems/Problems.tsx
+++ src/panel-triggers/components/Problems/Problems.tsx
@@ -729,16 +729,22 @@ const DEFAULT_OK_COLOR = 'rgb(56, 189, 113)';
 const DEFAULT_PROBLEM_COLOR = 'rgb(215, 0, 0)';
 
 function StatusCell(props: RTCell<ProblemDTO>, highlightNewerThan?: string) {
-  let status = props.value === '0' ? 'RESOLVED' : 'PROBLEM';
-  let color = props.value === '0' ? DEFAULT_OK_COLOR : DEFAULT_PROBLEM_COLOR;
-  if (props.original.manual_close === '1') {
-    status = 'RESOLVED';
-    color = DEFAULT_OK_COLOR;
-  }
+  let status;
+  let color;
+
   if (props.value === '1') {
     status = 'PROBLEM';
     color = DEFAULT_PROBLEM_COLOR;
+  } else {
+    status = 'RESOLVED';
+    color = DEFAULT_OK_COLOR;
   }
+
+  if (props.original.manual_close === '1' && props.original.value !== '1') {
+    status = 'RESOLVED';
+    color = DEFAULT_OK_COLOR;
+  }
+
   let newProblem = false;
   if (highlightNewerThan) {
     newProblem = isNewProblem(props.original, highlightNewerThan);
